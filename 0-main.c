54d4a6624b056427f07f2d53325d7865b23359f8
Xhibitbrown <kusiadubofour72@gmail.com>
2022-10-20 23:24:27 +0000

    2

--- 0-main.c
+++ 0-main.c
@@ -1,4 +1,4 @@
-#include "mian.h"
+#include <stdio.h> 
 
 /**
 * main - tests function that prints if integer is positive or negative
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-0x03. C - Debugging
+##0x03. C - Debugging
+
+###This project is all about learning about Debugging
